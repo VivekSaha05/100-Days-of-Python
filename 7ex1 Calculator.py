f97f58510256dec628be6c85e2ae326c5b3d257e
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-07 22:51:30 +0530

    Day7 (Simple Calculator)

---
+++ 7ex1 Calculator.py
@@ -0,0 +1,11 @@
+# Exercise 1 - Calculator (WITHOUT IF ELSE)
+# simple add,sub,mul,div
+
+a = 60
+b = 9
+
+print("the result of ",a, "+",b, "is :", a+b)
+print("the result of ",a, "-",b, "is :", a-b)
+print("the result of ",a, "*",b, "is :", a*b)
+print("the result of ",a, "/",b, "is :", a/b)
+print("the result of ",a, "%",b, "is :", a%b)
