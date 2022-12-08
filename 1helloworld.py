37e6cc1683931e0046750df8a1314111c63d82a7
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-08 15:35:37 +0530

    Day1 (Hello Python World!)

--- 1helloworld.py
+++ 1helloworld.py
@@ -1,11 +1,11 @@
-# 100 Days of code by CodewithHarry!!
+# 100 Days of code From CodewithHarry By Vivek_Saha!!!
 
-# Raplet can be used to run python file as it is easy to run and share.
 # python file is saved by .py file extension.
 # platform independent and easiest lang to learn
 # has one of the biggest lib.
-# can be integrate with with many lang i.e with C.
+# can be integrated with many lang i.e with C.
 # can be used in database, data science, Machine learning, web app, etc.
 
+# basic Boiler plate
 print("Hello Python World!!")
 print(5)
