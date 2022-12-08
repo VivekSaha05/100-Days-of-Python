078e0669b8d0dc8333ad5d9a05149e0263b9b05f
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-08 15:54:42 +0530

    Day7 (Calculator without user input)

---
+++ 07ex1 Calculator.py
@@ -0,0 +1,11 @@
+# Exercise 1 - Calculator (WITHOUT user input and if else)
+# simple add,sub,mul,div
+
+a = 60
+b = 9
+
+print("the result of ",a, "+",b, "is :", a+b)
+print("the result of ",a, "-",b, "is :", a-b)
+print("the result of ",a, "*",b, "is :", a*b)
+print("the result of ",a, "/",b, "is :", a/b)
+print("the result of ",a, "%",b, "is :", a%b)
