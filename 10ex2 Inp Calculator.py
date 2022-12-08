c5dd827146b020ade1b11e38d07d389083f26ec3
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-08 15:50:09 +0530

    Day10 (Calculator With User Input)

---
+++ 10ex2 Inp Calculator.py
@@ -0,0 +1,15 @@
+# Calculator Using User Input (But without If Else)
+# All Possible operation is going to be printed
+# Float Value is Supported
+
+print("************CALCULATOR************")
+print()
+
+a = input("Enter Your first Number : ")
+b = input("Enter Your Second Number : ")
+
+print("The Addition of the number is :", float(a) + float(b))
+print("The Subtraction of the number is :", float(a) - float(b))
+print("The Multiplication of the number is :", float(a) * float(b))
+print("The Division of the number is :", float(a) / float(b))
+print("The Modulus of the number is :", float(a) % float(b))
