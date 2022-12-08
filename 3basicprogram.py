4cf5e834af27193d6a66feaf3546de28e42dc84b
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-08 15:57:02 +0530

    Renamed Files

--- 3basicprogram.py
+++
@@ -1,7 +0,0 @@
-# Basic Understanding Of an Program.
-
-print("hello world")  # print is a function
-print(5)
-print("bye", 7)
-print(17*3)  # '+ , - , / , *' can be used for any simple operation
-
--- 6arthmoper.py
+++
@@ -1,9 +0,0 @@
-# Arithmetic Operator
-
-print(15+6)   # addition
-print(15-6)   # subtraction
-print(15*6)   # multiplication
-print(15**6)  # to the power
-print(15/6)   # float division
-print(15//6)  # div without point value
-print(15%6)   # remainder / Modulus
