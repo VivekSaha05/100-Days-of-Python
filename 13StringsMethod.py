134cf36d37b9fccf4a7a8146d7dcc716ac575bdb
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-12 16:53:59 +0530

    Day13 (String Methods)

---
+++ 13StringsMethod.py
@@ -0,0 +1,11 @@
+# STRINGS METHOD
+
+"""
+Python provides a set of built-in
+methods that we can use to alter and modify the strings.
+Strings are immutable.
+"""
+
+name = "VivekSaha"
+print(name.upper())  # uppercase
+print(name.lower())  # lowercase
