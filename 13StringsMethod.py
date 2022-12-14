90787493e7d1b80e9aaacc53adb7b0c62ce4c4ea
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-14 16:23:29 +0530

    Day13 (String Methods)

--- 13StringsMethod.py
+++ 13StringsMethod.py
@@ -16,5 +16,7 @@ print(name2.rstrip("!"))  # rstrip remove from end only
 name3 = "bibek"
 print(name3.replace("bibek", "vivek"))  # replace name3 with new name
 
-print(name2.split(" "))
+print(name2.split(" "))  # splits the word inside []
+
+print(name3.capitalize())  # will make 1st letter of word capital
 
