dbcb92e420c5658103c0d94bded8a585421e432a
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-17 22:09:01 +0530

    Day14 (If-Else)

--- 14If-else.py
+++ 14If-else.py
@@ -1,1 +1,13 @@
 # IF ELSE CONDITION
+
+"""
+if, if-else, if-else-elif, nested if-else-elif
+"""
+
+a = int(input("Enter Your age:"))
+print("Your age is", a)
+
+if a > 18:  # can a>18 inside ()
+    print("you can drive")
+else:
+    print("You can't drive")
