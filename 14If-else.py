3060f7b77c0742320aeda606832995f2fb132b30
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-17 22:41:51 +0530

    Day14 (If-Else)

--- 14If-else.py
+++ 14If-else.py
@@ -6,18 +6,20 @@ if, if-else, if-else-elif, nested if-else-elif
 Conditional operator :
 >, <, <=, >=, ==, != (! =)
 """
-a = int(input("Enter a number to check the satisfy:"))
-print(a == 18)
-print(a > 18)
-print(a < 18)
-print(a >= 18)
-print(a <= 18)
-print(a != 18)
+# a = int(input("Enter a number to check the satisfy:"))
+# print(a == 18)
+# print(a > 18)
+# print(a < 18)
+# print(a >= 18)
+# print(a <= 18)
+# print(a != 18)
 
 # If-else condition
 age = int(input("Enter Your age:"))
 print("Your age is", age)
 if age > 18:  # can add age>18 inside ()
     print("you can drive")
+elif age >= 18:
+    print("Kuddos...just passed")
 else:
     print("You can't drive")
