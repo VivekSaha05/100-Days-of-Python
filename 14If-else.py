8f6fde7dc6235c44c4dbd48b9ec2e134a69779a9
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-21 04:08:54 +0530

    Day14 (If-Else)

--- 14If-else.py
+++ 14If-else.py
@@ -15,7 +15,7 @@ print(a <= 18)
 print(a != 18)
 
 # If-else condition
-
+# ex1
 age = int(input("Enter Your age:"))
 print("Your age is", age)
 if age > 18:  # can add age>18 inside ()
@@ -25,4 +25,29 @@ elif age >= 18:  # elif condition
 else:
     print("You can't drive")
 
-    print("hello")
+# ex2
+num = int(input("Enter the Number for If-else-elif check:"))
+if num < 0:
+    print("Number is Negative")
+elif num == 0:
+    print("Null Value")
+elif num == 69:
+    print("Special Value")
+else:
+    print("Number is Positive")
+
+# Nested if Statement (If else under If else.....making tree inside it)
+
+num1 = int(input("Enter Your Number For Nested Check :"))
+if num < 0:
+    print("Number is Negative.")
+elif num > 0:
+    if num <= 10:
+        print("Number is Less than Or equal to 10")
+    elif 10 < num <= 20:
+        print("Number is in btw 10-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is Null")
+
