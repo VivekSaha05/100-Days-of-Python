36de87e9adfc29c63c4350c3559e9b6d2ba2cf31
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-24 00:25:12 +0530

    Day19 (Break And Continue)

---
+++ 19BreakandContinue.py
@@ -0,0 +1,14 @@
+# BREAK AND CONTINUE
+
+# BREAK - enables a prog to skip over a part of the code, terminates the very loop.
+# CONTINUE - will terminate the iteration
+
+for i in range(12):
+    if i == 10:
+        break  # will break the condition after it doesn't satisfy
+    print("5 X", i+1, "=", 5 * (i+1))
+
+for i in range(12):
+    if i == 10:
+        continue  # will skip the given if condition
+    print("5 X", i+1, "=", 5 * (i+1))
