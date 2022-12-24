ace2315ebec51763935a088fc31d33931884b2f1
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2022-12-24 23:24:31 +0530

    Day19 (Break And Continue)

--- 19BreakandContinue.py
+++ 19BreakandContinue.py
@@ -7,8 +7,20 @@
 #     if i == 10:
 #         break  # will break the condition after it doesn't satisfy
 #     print("5 X", i+1, "=", 5 * (i+1))  # if +1 not given then will start from 0
+#
+# for i in range(12):
+#     if i == 10:
+#         continue  # will skip the given if condition
+#     print("5 X", i+1, "=", 5 * i+1)
 
-for i in range(12):
-    if i == 10:
-        continue  # will skip the given if condition
-    print("5 X", i+1, "=", 5 * i+1)
+# continue - skip the iteration / condition
+# break - whole loop breaks
+
+# Do-while loop evault using break
+
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if i % 100 == 0:
+        break
