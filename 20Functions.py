95ad926592e14b01c6512bed3677b9d55038820b
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-05 19:19:55 +0530

    Day 20 (Functions)

--- 20Functions.py
+++ 20Functions.py
@@ -3,5 +3,7 @@
 """
 we are going to make a block of code and then using it later
 instead of creating that code again and again later.
+it will make the code clean and effective if we want to change anything
+later on.
 """
 
