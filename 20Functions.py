3d34c2198fe0f501d7b354a06ac30d6b1c670b63
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-05 19:35:07 +0530

    Day 20 (Functions)

--- 20Functions.py
+++ 20Functions.py
@@ -1,9 +1,20 @@
 # FUNCTION
 
 """
-we are going to make a block of code and then using it later
+>we are going to make a block of code and then using it later
 instead of creating that code again and again later.
-it will make the code clean and effective if we want to change anything
+>it will make the code clean and effective if we want to change anything
 later on.
 """
+def calmean(a, b):  # we will call this function
+    mean = (a*b)/(a+b)
+    print(mean)
+
+# ex:
+a = 5
+b = 6
+# mean1 = (a*b)/(a+b)
+# print(mean1)
+calmean(a, b)  # called function
+
 
