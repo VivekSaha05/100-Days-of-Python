ddd9f925d0450b0badd22b9f13af2e70f566d970
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-05 20:05:22 +0530

    Day 20 (Functions)

--- 20Functions.py
+++ 20Functions.py
@@ -1,16 +1,16 @@
 # FUNCTION
-
 """
 >we are going to make a block of code and then using it later
 instead of creating that code again and again later.
 >it will make the code clean and effective if we want to change anything
 later on.
 """
+
+# ex:
 def calmean(a, b):  # we will call this function
     mean = (a*b)/(a+b)
     print(mean)
 
-# ex:
 a = 5
 b = 6
 # mean1 = (a*b)/(a+b)
