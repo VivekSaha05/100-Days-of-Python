a37818dff9dab4c7a218696afcbc1a391bbd3f77
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-17 12:35:06 +0530

    Day 21 (Function Arguments)

--- 21FunctionArguments.py
+++ 21FunctionArguments.py
@@ -7,7 +7,9 @@ as a tuple or dictionary
 
 def average(a=2, b=4):  # default argument is 2 and 4 respectively
     print("the avrg is ", (a+b)/2)
-# average()
-# average(4)  # value of a=4 and b wil be default
 
+# average()  # will take default argument
+# average(4)  # value of a=4 and b wil be default
+# average(b=10, a=5)  # keyword argument (order will be defined)
+# average(4,10)  # required arguments (required if a and b is defined in function without default argument)
 
