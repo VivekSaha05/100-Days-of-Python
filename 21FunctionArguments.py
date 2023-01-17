1572f8fe4e134d0eb1c96f8d8d93932528121804
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-17 12:20:30 +0530

    Day 21 (Function Arguments)

--- 21FunctionArguments.py
+++ 21FunctionArguments.py
@@ -4,3 +4,10 @@
 passing an argument to a function
 as a tuple or dictionary
 """
+
+def average(a=2, b=4):  # default argument is 2 and 4 respectively
+    print("the avrg is ", (a+b)/2)
+# average()
+# average(4)  # value of a=4 and b wil be default
+
+
