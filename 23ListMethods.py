38851b12668ad0f287ae2955a86167809eff8eb1
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-18 23:40:45 +0530

    Day 23 (list Methods)

--- 23ListMethods.py
+++ 23ListMethods.py
@@ -2,6 +2,8 @@
 
 """
 methods to manipulate lists
+ex-
+list.sort(), reverse(), index(), count(), copy(), append(), insert(), extend()
 """
 
 lst = [1,2,3,4,5,6]
@@ -13,14 +15,18 @@ print(lst)
 num = [4,2,5,3,6,1,2,1,2,8,9,7]
 num.sort()  # will sort it in ascending order
 print(num)
+print(num.index(6))  # will print first occurance of that number
 num.reverse()  # will reverse it directly
 print(num)
 num.sort(reverse=True)  # will reverse + sort it in descending order
 print(num)
+print(num.count(1))  # will count it occurance
 
 # note - by default list is false i.e. is in ascending order. (chota se bada)
 
-colors = ["voilet", "indigo", "blue", "green"]
-colors.sort()
+colors = ["violet", "indigo", "blue", "green"]
+newlist = colors.copy()
+colors.sort()  # sorted wrt alphabets
 print(colors)
+print(newlist)
 
