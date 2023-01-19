bee7618955b17bf4e01e33cd58eef4a6c012b728
Vivek Saha <108818360+VivekSaha05@users.noreply.github.com>
2023-01-19 20:54:27 +0530

    Day 24 (Tuples)

---
+++ 24Tuples.py
@@ -0,0 +1,8 @@
+# TUPLES
+
+"""
+similar to list but tuples cant be change i.e insertition, deletetion, all list methods
+"""
+
+tup = (1, 5)  # Tuples are in () brackts whereas list have []
+print(type(tup), tup)
